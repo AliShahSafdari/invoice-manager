9d4569b90e0241aeba53ad4629cd72c213455fa4
Alishah Safdari <alishahsafdari905@gmail.com>
2024-07-30 19:49:41 +0430

    Modified DeleteModal

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default async function Home({ searchParams }) {
     <div className="flex justify-center min-h-[82vh]">
       <section className=" w-full px-2 max-w-[1000px]">
         <div className=" flex justify-between">
-          <h3 className=" text-2xl font-semibold"> Invoice Manager</h3>
+          <h3 className=" text-2xl font-semibold"> Invoice New</h3>
           <CreateInvoice />
         </div>
         <Separator className="my-2 border-b-[2px] border-color-light-blue" />
